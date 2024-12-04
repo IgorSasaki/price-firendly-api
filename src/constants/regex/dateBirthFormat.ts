8ae2d2d43cb33a200930ee1772fc6ba2d8a7dc21
IgorSasaki <igor-sasaki@hotmail.com>
2024-12-04 18:15:39 -0300

    feat(constants regex): implement constants with regex validation

---
+++ src/constants/regex/dateBirthFormat.ts
@@ -0,0 +1,1 @@
+export const REGEX_TO_DATE_BIRTH_FORMAT = /^\d{4}-\d{2}-\d{2}$/
---
+++ src/constants/regex/generalEmailFormat.ts
@@ -0,0 +1,2 @@
+export const REGEX_TO_GENERAL_EMAIL_FORMAT =
+  /^[a-zA-Z0-9._%+-]+@(?!.*my\.id)([a-zA-Z0-9-]+\.)+[a-zA-Z]{2,}$/
