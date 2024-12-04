3ce7d1196afca4e73410932741a6cdf1b951cad9
IgorSasaki <igor-sasaki@hotmail.com>
2024-12-04 18:14:07 -0300

    feat(models user): implement logic model for user

---
+++ src/models/user.ts
@@ -0,0 +1,13 @@
+export interface User {
+  country: string
+  createdAt: string
+  dateBirth: string
+  email: string
+  isConfirm: boolean
+  lastAccessAt: string
+  name: string
+  password: string
+  phone: string
+  updatedAt: string
+  userId: string
+}
